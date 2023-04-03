5ff485917df4e55b31e37b9c88c9ae3b6bf056f3
Erik Antonyan <erik.antonyan1994@gmail.com>
2023-04-03 08:35:58 +0400

    Add lazy column

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,17 +22,116 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//
-//                }
-
-                TextCompose(modifier = Modifier)
+                MyApp(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = MaterialTheme.colorScheme.onBackground)
+                )
+                // TextCompose(modifier = Modifier)
             }
         }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    // A surface container using the 'background' color from the theme
+//    Surface(
+//        modifier = modifier,
+//        color = MaterialTheme.colorScheme.primary
+//    ) {
+//
+//    }
+
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(modifier = modifier) {
+                shouldShowOnboarding = false
+            }
+        } else {
+            Greetings(modifier = modifier)
+        }
+    }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(
+        modifier = modifier
+            .padding(vertical = 4.dp)
+    ) {
+        items(items = names) {
+            Greeting(name = it)
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier, onClick: () -> Unit) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!", color = MaterialTheme.colorScheme.background)
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onClick
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
+
+    Surface(
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .fillMaxWidth(),
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        Row(modifier = Modifier.padding(24.dp)/*.fillMaxWidth()*/) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            ButtonComposable(
+                text = if (expanded) "Show Less" else "Show More",
+                color = MaterialTheme.colorScheme.onBackground
+            ) {
+                expanded = !expanded
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonComposable(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color,
+    onClick: () -> Unit
+) {
+    ElevatedButton(
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(containerColor = color),
+        onClick = onClick
+    ) {
+        Text(text = text)
     }
 }
 
@@ -39,7 +141,6 @@ fun TextCompose(modifier: Modifier) {
         modifier = Modifier
             .background(Color.Green)
             .fillMaxSize()
-            .padding(top = 15.dp)
             .border(width = 5.dp, color = Color.Black)
             .padding(5.dp)
             .border(width = 5.dp, color = Color.White)
